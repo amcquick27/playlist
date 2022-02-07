4f9fa9443c1dd9af7fac398d0cecb2959c9dbd5e
Glitch (outgoing-mixed-microwave) <none>
2022-02-07 02:24:04 +0000

    Created an array to store the names of the songs, their artists, pictures and links.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 // Complete Day 1 goals here
+let mySongs=["WISH","Butterflies","Nights"];
+let songArtists=["Denzel Curry","MAX","Frank Ocean"];
+let songImages=[
+  "<img= 'https://images.genius.com/86e21c57ddf05f2cf02a69f8a77255d3.300x300x1.png'/>",
+  "<img= 'https://images.genius.com/77586a887ddbcb821c4c8b42d9e9770c.1000x1000x1.png'/>",
+  "<img= 'https://images.genius.com/626ddf4c88de200d9487bb42449d1ae3.1000x1000x1.png'/>"
+               ];
+let songLinks=[
+"<a href='https://youtu.be/pEZDFv6DNQU'></a>",
+"<a href='https://www.youtube.com/watch?v=Sa7jhh5_dQQ'></a>",  
+"<a href='https://www.youtube.com/watch?v=r4l9bFqgMaQ'></a>"   
+];
 
 function displaySongInfo() {
   // Complete the Day 2 goals inside this function
